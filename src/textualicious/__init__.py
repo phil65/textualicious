f4451f0bedc9ae3eef523724ccb29d0f9992ee91
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-04 04:17:03 +0100

    chore: add help screen

--- src/textualicious/__init__.py
+++ src/textualicious/__init__.py
@@ -3,5 +3,6 @@ __version__ = "0.0.1"
 from textualicious.dataclass_table import DataClassTable
 from textualicious.dataclass_viewer import DataClassViewer
 from textualicious.log_widget import LoggingWidget
+from textualicious.help_screen import HelpScreen
 
-__all__ = ["DataClassTable", "DataClassViewer", "LoggingWidget"]
+__all__ = ["DataClassTable", "DataClassViewer", "HelpScreen", "LoggingWidget"]
