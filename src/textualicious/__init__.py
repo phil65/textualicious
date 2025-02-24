42faf57848abbe0540695359d1d84af8cca3173d
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-24 17:05:53 +0100

    feat: add show_path helper and UniversalDirectoryTree

--- src/textualicious/__init__.py
+++ src/textualicious/__init__.py
@@ -4,5 +4,13 @@ from textualicious.dataclass_table import DataClassTable
 from textualicious.dataclass_viewer import DataClassViewer
 from textualicious.log_widget import LoggingWidget
 from textualicious.help_screen import HelpScreen
+from textualicious.upath_tree import UniversalDirectoryTree, show_path
 
-__all__ = ["DataClassTable", "DataClassViewer", "HelpScreen", "LoggingWidget"]
+__all__ = [
+    "DataClassTable",
+    "DataClassViewer",
+    "HelpScreen",
+    "LoggingWidget",
+    "UniversalDirectoryTree",
+    "show_path",
+]
