6aee6582a3576e0e07bca8167b2fd7b6f3f0b4d7
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-24 17:29:47 +0100

    chore: avoid upath import

--- src/textualicious/upath_tree.py
+++ src/textualicious/upath_tree.py
@@ -2,44 +2,11 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from textual.widgets import DirectoryTree
 from upath import UPath
-
-
-if TYPE_CHECKING:
-    import os
 
 
 class UniversalDirectoryTree(DirectoryTree):
     """DirectoryTree widget supporting local and remote filesystems via upath."""
 
     PATH = UPath
-
-
-def show_path(path: str | os.PathLike[str]) -> None:
-    """Show the contents of a path in a UniversalDirectoryTree widget."""
-    from textual.app import App, ComposeResult
-    from textual.widgets import Footer, Header
-
-    path_obj = UPath(path)
-
-    class DemoApp(App):
-        """Demo app showing the UniversalDirectoryTree widget."""
-
-        def __init__(self, path: str | os.PathLike[str]) -> None:
-            super().__init__()
-            self.path = UPath(path)
-
-        def compose(self) -> ComposeResult:
-            yield Header()
-            yield UniversalDirectoryTree(self.path)
-            yield Footer()
-
-    app = DemoApp(path_obj)
-    app.run()
-
-
-if __name__ == "__main__":
-    show_path("github://phil65:mknodes@main")
